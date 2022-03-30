0165ff60a80f044924084aad7b87e830467ea177
antares0982@gmail.com <Antares0982@gmail.com>
2022-03-31 01:02:05 +0800

    :tada: first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # reversiProxy
-黑白棋代理
+黑白棋后端代理
+
+主体代码由[OrangeX4](https://github.com/OrangeX4)提供，配合[reversi前端](https://github.com/OrangeX4/Reversi)使用即可
+
+配置文件`config.ini`格式：
+```
+[config]
+ip=x.x.x.x
+```
---
+++ listener.py
@@ -0,0 +1,40 @@
+
+
+#!/usr/bin/python3
+from configparser import ConfigParser
+
+import requests
+from flask import Flask, Response, request
+
+cfgparser = ConfigParser()
+cfgparser.read("config.ini")
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request(resp):
+    """跨域支持."""
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Method'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return resp
+
+
+@app.route('/ai_list', methods=['GET'])
+def get_ai_map():
+    ans = requests.get(
+        f"http://{cfgparser['config']['ip']}:7685/ai_list").content
+    return Response(ans, mimetype='application/json')
+
+
+@app.route('/ai_api', methods=['POST'])
+def ai_api():
+    ans = requests.post(f"http://{cfgparser['config']['ip']}:7685/ai_api",
+                        json=request.json).content
+    return Response(ans, mimetype='application/json')
+
+
+# 挂载
+if __name__ == '__main__':
+    app.run(debug=False, port=7685, host='0.0.0.0')
